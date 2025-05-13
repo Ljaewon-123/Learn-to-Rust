d70b515d229ffc6af0ef89d67e014f56110c64d7
Ljaewon-123 <jml6388@naver.com>
2025-05-13 21:07:45 +0900

    clone

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let mut s = String::from("hello"); // 필요한 만큼만 메모리 요구 
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
 
-    s.push_str(", world!"); // push_str()이 문자열에 리터럴을 추가합니다
-
-    println!("{}", s); // 이 줄이 `hello, world!`를 출력합니다
+    println!("s1 = {}, s2 = {}", s1, s2);
 
 } // 스코프 가 끝나면 `drop`이 호출되어 메모리가 해제됨
 
+// 힙 데이터까지 복사
