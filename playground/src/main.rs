0bccb33ae958be3a6ca16a3010a8559e0868dd48
Ljaewon-123 <jml6388@naver.com>
2025-05-13 22:06:38 +0900

    struct

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,12 +1,28 @@
 fn main() {
-    let mut s = String::from("hello");
-
-    let r1 = &s; // 문제없음
-    let r2 = &s; // 문제없음
-    println!("{} and {}", r1, r2);
-    // 이 지점 이후로 변수 r1과 r2는 사용되지 않습니다
-
-    let r3 = &mut s; // 문제없음
-    println!("{}", r3);
-
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
 }
+
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+
