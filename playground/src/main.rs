1136fb14f2ee6bd226c465b8800d623893263ea9
Ljaewon-123 <jml6388@naver.com>
2025-05-13 21:40:36 +0900

    refs

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,23 +1,12 @@
 fn main() {
-    let s = String::from("hello");  // s가 스코프 안으로 들어옵니다
+    let mut s = String::from("hello");
 
-    takes_ownership(s);             // s의 값이 함수로 이동됩니다...
-                                    // ... 따라서 여기서는 더 이상 유효하지 않습니다
-    // println!("{}", s); // This code not compoile
-    let x = 5;                      // x가 스코프 안으로 들어옵니다
+    let r1 = &s; // 문제없음
+    let r2 = &s; // 문제없음
+    println!("{} and {}", r1, r2);
+    // 이 지점 이후로 변수 r1과 r2는 사용되지 않습니다
 
-    makes_copy(x);                  // x가 함수로 이동될 것입니다만,
-                                    // i32는 Copy이므로 앞으로 계속 x를
-                                    // 사용해도 좋습니다
+    let r3 = &mut s; // 문제없음
+    println!("{}", r3);
 
-} // 여기서 x가 스코프 밖으로 벗어나고 s도 그렇게 됩니다. 그러나 s의 값이 이동되었으므로
-  // 별다른 일이 발생하지 않습니다.
-
-fn takes_ownership(some_string: String) { // some_string이 스코프 안으로 들어옵니다
-    println!("{}", some_string);
-} // 여기서 some_string이 스코프 밖으로 벗어나고 `drop`이 호출됩니다.
-  // 메모리가 해제됩니다.
-
-fn makes_copy(some_integer: i32) { // some_integer가 스코프 안으로 들어옵니다
-    println!("{}", some_integer);
-} // 여기서 some_integer가 스코프 밖으로 벗어납니다. 별다른 일이 발생하지 않습니다.
+}
