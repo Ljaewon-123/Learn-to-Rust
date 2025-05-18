e71d09aebba65853f61dd83fd41c937e020c4799
Ljaewon-123 <jml6388@naver.com>
2025-05-18 18:30:47 +0900

    if else

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -17,3 +17,16 @@ fn main() {
         },
     };
 }
+
+// using if let to simplify the match statement
+fn main() {
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
