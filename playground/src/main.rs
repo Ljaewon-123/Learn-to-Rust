cfc3b44009e0ea5f623f0e866a9d2ca440ec0a57
Ljaewon-123 <jml6388@naver.com>
2025-05-21 17:36:50 +0900

    closer

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,10 +1,52 @@
-use std::fs::File;
-use std::io::{self, Read};
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    let mut username = String::new();
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
 
-    File::open("hello.txt")?.read_to_string(&mut username)?;
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
 
-    Ok(username)
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
 }
